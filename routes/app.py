ef491259dfa1d568ae625337af8d47926a844f24
ThunderGer <jaames11@hotmail.com>
2023-04-20 18:27:25 -0600

    Probando con la extracción de la DB

--- routes/app.py
+++ routes/app.py
@@ -10,6 +10,13 @@ routes = APIRouter()
 @routes.post('/probando')
 def postTest(text: testFile):
     print(text.id)
+    filesave = []
+    file = conn.local.files.find_one({"_id": text.id})
+    filename = file["name"]
+    filebyte = file["data"]
+    filesave.append(filename)
+    with open(f'./document/{filename}', 'wb') as f:
+            f.write(filebyte)
     return 'ok'
 
 @routes.get('/probando')
